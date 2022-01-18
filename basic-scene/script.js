ec56b6c00dbeea33f03a16f74ccc695096026e71
APerez89 <alexperez689@gmail.com>
2022-01-18 12:03:49 -0500

    new classes created for the first scene

--- basic-scene/script.js
+++ basic-scene/script.js
@@ -1,2 +1,23 @@
+// Scene consists of objects & camera
+
 // New Scene
 const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1,);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000});
+const mesh = new THREE.Mesh(geometry, material);
+
+// **Important** Add the mesh to the scene
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+
